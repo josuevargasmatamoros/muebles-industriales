f16659069db65b8e0981f2fcf200f30c079c183b
josuevargasmatamoros <109043534+josuevargasmatamoros@users.noreply.github.com>
2026-02-23 10:35:29 -0600

    imgs cloudinary next config

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,14 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // Agrega aquí los dominios de imágenes externas que uses
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      // Agrega más dominios según necesites
     ],
   },
-  // Optimizaciones para producción
   compress: true,
   poweredByHeader: false,
 };
